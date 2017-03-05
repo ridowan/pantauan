8b4b70aa91571ca551dd9d15fbe0c2633677e025
ridowan <ridowann@gmail.com>
2017-03-05 12:27:11 +0700

    Update func_config.php

--- config/func_config.php
+++ config/func_config.php
@@ -1,10 +1,10 @@
 <?php
 //database connection details
-$host = "192.168.89.15";
-$user = "postgres";
-$pass = "pastigana2016";
-$db   = "hotspot";
-$port = "5432";
+$host = ""; //your addres
+$user = ""; //your user
+$pass = ""; //your password
+$db   = ""; //your name db
+$port = ""; //your port service db
 
 $connect = pg_connect("host=$host user=$user password=$pass dbname=$db port=$port") or die ("Database or host Error".pg_last_error());
 
